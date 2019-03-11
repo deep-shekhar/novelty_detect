d0425e927939929fd69fc5934dc6ca85c5a57ec8
deep <deshkhr54@gmail.com>
2019-03-12 03:10:35 +0530

    increased epochs

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from utils import pp, visualize, to_json, show_all_variables
 import tensorflow as tf
 
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 150, "Epoch to train [25]")
+flags.DEFINE_integer("epoch", 250, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("attention_label", 1, "Conditioned label that growth attention of training label [1]")
